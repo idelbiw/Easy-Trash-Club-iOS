a445f9ea52fa0f89201658b85e613c369ddf37bc
James McDougall <jameswiliammcdougall@gmail.com>
2020-06-11 17:44:42 -0500

    Finished Registration Storyboard Basic UI Layout

---
+++ EasyTrashClub/EasyTrashClub/Controllers/ViewControllers/GetStarted/UserPasswordViewController.swift
@@ -0,0 +1,43 @@
+//
+//  UserPasswordViewController.swift
+//  EasyTrashClub
+//
+//  Created by Lambda_School_loaner_226 on 6/11/20.
+//  Copyright © 2020 EasyTrashClub. All rights reserved.
+//
+
+import UIKit
+
+class UserPasswordViewController: UIViewController {
+
+    //MARK: -IBOutlets
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var userConfirmPasswordTextField: UITextField!
+    @IBOutlet weak var userPasswordNextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - Helper Functions
+    
+    func configureNextButton() {
+        userPasswordNextButton.layer.cornerRadius = userPasswordNextButton.frame.size.height / 2
+    }
+    
+    //MARK: - Action Functions
+    
+    @IBAction func userPasswordTextFieldEdited(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func userConfirmPasswordTextFieldEdited(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func userPasswordNextButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
