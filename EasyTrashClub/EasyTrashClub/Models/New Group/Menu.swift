49d9bec0ffc87898df89d4e5038bf355af26a0ad
James McDougall <jameswiliammcdougall@gmail.com>
2020-06-15 23:00:44 -0500

    Initial Menu Wireframing complete

---
+++ EasyTrashClub/EasyTrashClub/Models/New Group/Menu.swift
@@ -0,0 +1,13 @@
+//
+//  Menu.swift
+//  EasyTrashClub
+//
+//  Created by Lambda_School_loaner_226 on 6/14/20.
+//  Copyright © 2020 EasyTrashClub. All rights reserved.
+//
+
+import Foundation
+
+struct Menu {
+    var title: String
+}
